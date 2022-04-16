9c3e4594408e2df8e67bcfa4f14c0595db088bc4
akjilani120 <abdulkaderjilani120@gmail.com>
2022-04-17 01:17:33 +0600

    navbar style complete

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import CustomLink from '../CustomLink/CustomLink';
+import { Link } from 'react-router-dom';
+import logoImg from '../img/main logo.png'
 import './Header.css'
 const Header = () => {
     return (
         <div>
-            <Navbar className='navbar' collapseOnSelect expand="lg" bg="white" variant="light">
+            <Navbar sticky='top' className='navbar' collapseOnSelect expand="lg" bg="white" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/home"  ><img className='logo-img' src={logoImg} alt="" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto">
@@ -19,7 +21,7 @@ const Header = () => {
                             </Nav.Link>
                         </Nav>
                         <Nav className="">
-                        <Nav.Link><CustomLink className="navigataion-list nav-login " to='/login'>Login</CustomLink></Nav.Link>
+                        <Nav.Link><CustomLink className="navigataion-list nav-login " to='/login'>Login </CustomLink></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
