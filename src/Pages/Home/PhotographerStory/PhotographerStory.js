66be9feeea958444a2b340e72c139f8f29150157
akjilani120 <abdulkaderjilani120@gmail.com>
2022-04-17 03:05:35 +0600

    photographer story component complete

--- src/Pages/Home/PhotographerStory/PhotographerStory.js
+++ src/Pages/Home/PhotographerStory/PhotographerStory.js
@@ -7,7 +7,8 @@ const PhotographerStory = () => {
            <Container>
                <div className="photographerstory">
                    <div className='story-Details'>
-                        <h1>We're Gleam a small and enthusiastic photography studio based in New York</h1>
+                        <h1 className='story-title'>We're great a small and enthusiastic photography  based in New York</h1>
+                        <p className='story-about'>A photographer is someone who is skilled at taking pictures or photographs with a camera. An amateur photographer takes pictures for their own use. A professional photographer takes pictures as all or part of their job. Many people hire a photographer for their wedding, debutante party or other special events</p> 
                    </div>
                </div>
            </Container>
