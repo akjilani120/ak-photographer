ec5597d870f8724fc893949289a94231aa956009
akjilani120 <abdulkaderjilani120@gmail.com>
2022-04-17 22:57:22 +0600

    sign In component style finishe

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigate=useNavigate()
+    const handleSignIn=() =>{
+        navigate("/signin")
+    }
     return (
         <div>
             <h1>I am login page</h1>
+            <button onClick={handleSignIn} className='btn btn-primary'>Have you account</button>
         </div>
     );
 };
--- src/Pages/Login/SignIn/SignIn.js
+++ src/Pages/Login/SignIn/SignIn.js
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import { Button, Form } from 'react-bootstrap';
+import './SignIn.css'
 const SignIn = () => {
     return (
-        <div>
-            <h1>I am signIN pages</h1>
+        <div className='container sign-header '>
+           <div className="form-header ">
+               <h1 className=''>Please Sign In</h1>
+           <Form className=''>
+                <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
+                    <Form.Label className=''>Name</Form.Label>
+                    <Form.Control type="Name" placeholder="Enter Name" required/>                   
+                </Form.Group>
+                <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email"  required/>                   
+                </Form.Group>
+
+                <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3 text-start" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button className='px-5 sign-btn' variant="primary" type="submit">
+                    Sign In
+                </Button>
+            </Form>
+           </div>
         </div>
     );
 };
